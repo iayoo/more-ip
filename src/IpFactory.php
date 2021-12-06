60e4d11a570051676ef6db93f5cdfd97d5f9bc11
Iayoo <vilnius94@163.com>
2021-12-06 09:36:00 +0800

    # IP 工厂类

---
+++ src/IpFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Iayoo\IP;
+
+
+abstract class IpFactory
+{
+    /** @var string 经度 */
+    public $longitude;
+    /** @var string 纬度 */
+    public $latitude;
+    /** @var string ip */
+    public $ip;
+
+    /**
+     * @return string
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param string $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param string $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+        return $this;
+    }
+    
+    
+}
