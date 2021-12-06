fd8bab7a25d4e282b14914bb31b3eba7d66d0b27
Iayoo <vilnius94@163.com>
2021-12-06 11:59:52 +0800

    # 修改淘宝ip

--- src/source/TaoBaoApi.php
+++ src/source/TaoBaoApi.php
@@ -29,8 +29,11 @@ class TaoBaoApi extends \Iayoo\IP\IpFactory
     public function getIp($ip)
     {
         $this->ip = $ip;
-        $query = http_build_query(['accessKey'=>$this->clientKey,'ip'=>$ip]);
-        // ?accessKey=alibaba-inc
+        $queryArr = ['ip'=>$ip];
+        if (!empty($this->clientKey)){
+            $queryArr['accessKey'] = $this->clientKey;
+        }
+        $query = http_build_query($queryArr);
         $res = $this->send("GET","{$this->uri}?{$query}",[]);
         $json = json_decode($res,true);
         if ($json && isset($json['code']) && $json['code'] === 0){
