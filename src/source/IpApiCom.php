5ce6f7f84ac64f11bec4957a298f550e00d8b9a1
Iayoo <vilnius94@163.com>
2021-12-06 11:15:47 +0800

    # 新增ipApi支持

---
+++ src/source/IpApiCom.php
@@ -0,0 +1,25 @@
+<?php
+namespace Iayoo\IP\source;
+
+use Iayoo\IP\IpFactory;
+
+class IpApiCom extends IpFactory
+{
+
+    protected $uri = "http://ip-api.com/json/";
+
+    public function getIp($ip)
+    {
+        $this->ip = $ip;
+        $res = $this->send("GET","{$this->uri}{$ip}?lang=zh-CN",[]);
+        $json = json_decode($res,true);
+        if ($json && isset($json['status']) && $json['status'] == 'success'){
+            $this->setCountry(isset($json['country'])?$json['country']:'');
+            $this->setProvince(isset($json['regionName'])?$json['regionName']:'');
+            $this->setCity(isset($json['city'])?$json['city']:'');
+            $this->setLatitude(isset($json['lat'])?(string)$json['lat']:'');
+            $this->setLongitude(isset($json['lon'])?(string)$json['lon']:'');
+        }
+        return $this;
+    }
+}
